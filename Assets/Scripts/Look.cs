4cc50409f05bc6d877eecf7db46cbbffb6e1ce21
ViliusKk <vilius.kuokstas@gmail.com>
2025-02-02 14:19:56 +0200

    Map

---
+++ Assets/Scripts/Look.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        var mouseX = Input.GetAxis("Mouse X");
+        var mouseY = Input.GetAxis("Mouse Y");
+    }
+}
