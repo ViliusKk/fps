dc18e1bb2208073ac8dbf8ac73937a4973285905
ViliusKk <vilius.kuokstas@gmail.com>
2025-02-02 15:29:25 +0200

    Shoot script

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //bullet.transform.position += 
+        }
+    }
+}
