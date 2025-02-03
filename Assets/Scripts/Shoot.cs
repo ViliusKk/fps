b9a0f4dc5d5fca5d887afaf0158cb0773fec0ee9
ViliusKk <vilius.kuokstas@gmail.com>
2025-02-03 19:24:58 +0200

    Update Shoot.cs

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -11,12 +11,11 @@ public class Shoot : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            //bullet.transform.position += 
+            
         }
     }
 }
