488439db9aeb38b65efbb158e0bf4e80e4e3210d
ViliusKk <vilius.kuokstas@gmail.com>
2025-02-09 15:28:26 +0200

    Enemy script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform target;
+    
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
