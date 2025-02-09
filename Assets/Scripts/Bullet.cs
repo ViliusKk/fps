ef5cb6172749ece1f8d6859b21bf5ae08e139a6f
ViliusKk <vilius.kuokstas@gmail.com>
2025-02-09 13:56:55 +0200

    Bullet script

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20;
+    public float lifeTime = 3;
+    public Vector2 damageRange = new Vector2(10f, 20f);
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void onCollisionEnter2D(Collision2D collision)
+    {
+        // todo: deal damage
+        print($"Hit {collision.gameObject.name} for {Random.Range(damageRange.x, damageRange.y)} damage");
+        
+        Destroy(gameObject);
+    }
+}
